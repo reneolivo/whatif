4c5e570178156010c8d43596c9922cbf77d3a257
René Olivo <minet.ws@gmail.com>
2016-09-26 15:16:54 -0400

    add login use case

--- spec/whatif.spec.js
+++ spec/whatif.spec.js
@@ -19,4 +19,106 @@ describe('WhatIf', () => {
 
     expect(result instanceof WhatIfPromise).toBe(true);
   });
+
+  describe('use cases', () => {
+    describe('loging in', () => {
+      let user;
+      let theUserLogins;
+      let redirectTheUserToTheDashboard;
+      let displayAnErrorMessage;
+
+      beforeEach(() => {
+        user = { id: 1, name: 'Jon Snow' };
+
+        theUserLogins = jasmine.createSpy('theUserLogins');
+        redirectTheUserToTheDashboard = jasmine.createSpy('redirectTheUserToTheDashboard');
+        displayAnErrorMessage = jasmine.createSpy('displayAnErrorMessage');
+      });
+
+      describe('happy case', () => {
+        beforeEach(() => {
+          theUserLogins.and.returnValue(user);
+          redirectTheUserToTheDashboard.and.returnValue(true);
+
+          whatIf(theUserLogins)
+          .then(redirectTheUserToTheDashboard)
+          .otherwise(displayAnErrorMessage);
+        });
+
+        it('should call theUserLogins', () => {
+          expect(theUserLogins).toHaveBeenCalled();
+        });
+
+        it('should call redirectTheUserToTheDashboard', (next) => {
+          setTimeout(() => {
+            expect(redirectTheUserToTheDashboard).toHaveBeenCalledWith(user);
+            next();
+          }, 0);
+        });
+
+        it('should not call displayAnErrorMessage', (next) => {
+          setTimeout(() => {
+            expect(displayAnErrorMessage).not.toHaveBeenCalled();
+            next();
+          }, 0);
+        });
+      });
+
+      describe('login failure', () => {
+        const failureMessage = 'Login failure';
+
+        beforeEach(() => {
+          theUserLogins.and.throwError(failureMessage);
+          redirectTheUserToTheDashboard.and.returnValue(true);
+
+          whatIf(theUserLogins)
+          .then(redirectTheUserToTheDashboard)
+          .otherwise(displayAnErrorMessage);
+        });
+
+        it('should not call redirectTheUserToTheDashboard', (next) => {
+          setTimeout(() => {
+            expect(redirectTheUserToTheDashboard).not.toHaveBeenCalled();
+            next();
+          }, 0);
+        });
+
+        it('should call displayAnErrorMessage', (next) => {
+          setTimeout(() => {
+            expect(displayAnErrorMessage)
+            .toHaveBeenCalledWith(new Error(failureMessage));
+            next();
+          }, 0);
+        });
+      });
+
+      describe('login success, redirect to dashboard failure', () => {
+        const failureMessage = 'Could not redirect';
+
+        beforeEach(() => {
+          theUserLogins.and.returnValue(user);
+          redirectTheUserToTheDashboard.and.throwError(failureMessage);
+
+          whatIf(theUserLogins)
+          .then(redirectTheUserToTheDashboard)
+          .otherwise(displayAnErrorMessage);
+        });
+
+        it('should call redirectTheUserToTheDashboard', (next) => {
+          setTimeout(() => {
+            expect(redirectTheUserToTheDashboard).toHaveBeenCalledWith(user);
+            next();
+          }, 0);
+        });
+
+        it('should call displayAnErrorMessage', (next) => {
+          setTimeout(() => {
+            expect(displayAnErrorMessage)
+            .toHaveBeenCalledWith(new Error(failureMessage));
+            next();
+          }, 0);
+        });
+      });
+    })
+  });
 });
